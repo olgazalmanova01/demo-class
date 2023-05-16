34a2815f0d7b656f173cf349efa5869f55a3207b
“Olga <olgazalmanova085@gmail.com>
2023-05-16 20:34:00 +0300

    add class Main and create objects

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Card
+        Card card1 = new Card();
+        card1.title = "Пылесос";
+        card1.url = "https://www.website.com";
+
+        Description description1 = new Description();
+        description1.price = 500;
+        description1.description = "Моющий, мощный, LG";
+
+        card1.description = description1;
     }
 }
+
